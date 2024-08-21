649d4d55fd96119e6a4290ae4cc5a5de0e75040a
sushmitaghosh23 <sushmitag464@gmail.com>
2024-08-21 19:02:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const AccountId=12345
+let accountEmail="sushmitag@gmail.com"
+var accountPassword="12367"
+accountCity="Jaipur"
+let accountState;
+
+console.table([AccountId,accountEmail,accountPassword,accountCity,accountState])
