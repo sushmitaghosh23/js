077e15c5af9f9919eee3a503313fb246cf40d36d
Sushmita Ghosh <sushmitag464@gmail.com>
2024-08-22 18:02:16 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+let marks = "90abcd"
+console.log(typeof marks);
+
+// so when we are taking input as 90 we are getting the output as 'number' but when we are taking input "90" we are getting output as string so to overcome this  we need conversionoperation;
+
+let valueofmarks = Number(marks)
+console.log(typeof valueofmarks);
+// if we take the input as "90abc" we will get output as NaN(NOT A NUMBER) 
+console.log(valueofmarks)
+
+// "90" => 33
+// "90abcd" => NaN
+// true=>1 & false=>0
+
+//boolean
+//1=>true & 0=>false
+//""(emptystring) => false
+//"Sushmita" => true
+
+
+
+
+
