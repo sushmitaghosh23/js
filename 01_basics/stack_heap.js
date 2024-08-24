c3ba37c6cd900fcf809f34d0951e04bf87d95a7d
Sushmita Ghosh <sushmitag464@gmail.com>
2024-08-24 20:26:43 +0000

    stack and heap memory

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,21 @@
+let name1="sushmita"
+
+let anothername="name1"
+anothername="sneha"
+
+console.log(name1);
+console.log(anothername);
+
+let user1={
+    email:"sushmita@gmail.com",
+    upi:"upi@ybl"
+
+}
+let user2=user1;
+
+user2.email="sushmita23@gmail.com"
+console.log(user1.email);
+console.log(user1.email);
+
+
+
