a7775960df05d3550bbf9bc38f69ae854906273e
Sushmita Ghosh <sushmitag464@gmail.com>
2024-08-28 19:02:51 +0000

    date_and_times

---
+++ 01_basics/date_and_times.js
@@ -0,0 +1,18 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toUTCString());
+
+let mydate1 = new Date(2024,0,28)
+console.log(mydate1.toDateString());
+
+
+
+
+
+
