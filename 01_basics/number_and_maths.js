0fbb7f88f1a2719f6bdf1dcbeedb8de0cf752e3d
Sushmita Ghosh <sushmitag464@gmail.com>
2024-08-28 18:07:42 +0000

    number_and_maths

---
+++ 01_basics/number_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 40
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+// const othernumber = 56.789
+// console.log(othernumber.toPrecision(1));
+// const hundreds = 100000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(20.29));
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+console.log(Math.floor(Math.random()*10+1));
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
